52a576292974e11b2547561de14c2b37ca4a6429
dudv <dudv@safin.vn>
2022-06-02 08:57:20 +0700

    f

--- lib/themes/styles.dart
+++ lib/themes/styles.dart
@@ -83,6 +83,30 @@ mixin Styles {
     }
   }
 
+  static Map<int, Color> getSwatch(Color color) {
+    final hslColor = HSLColor.fromColor(color);
+    final lightness = hslColor.lightness;
+
+    const lowDivisor = 6;
+    const highDivisor = 5;
+
+    final lowStep = (1.0 - lightness) / lowDivisor;
+    final highStep = lightness / highDivisor;
+
+    return {
+      50: (hslColor.withLightness(lightness + (lowStep * 5))).toColor(),
+      100: (hslColor.withLightness(lightness + (lowStep * 4))).toColor(),
+      200: (hslColor.withLightness(lightness + (lowStep * 3))).toColor(),
+      300: (hslColor.withLightness(lightness + (lowStep * 2))).toColor(),
+      400: (hslColor.withLightness(lightness + lowStep)).toColor(),
+      500: (hslColor.withLightness(lightness)).toColor(),
+      600: (hslColor.withLightness(lightness - highStep)).toColor(),
+      700: (hslColor.withLightness(lightness - (highStep * 2))).toColor(),
+      800: (hslColor.withLightness(lightness - (highStep * 3))).toColor(),
+      900: (hslColor.withLightness(lightness - (highStep * 4))).toColor(),
+    };
+  }
+
   static const Color colorMain = Color(0xffFC9220);
   static const Color colorMain2 = Color(0xffFFEEDC);
   static const Color blue = Color(0xff3D9AE2);
@@ -190,4 +214,5 @@ mixin Styles {
   static const Color grey55 = Color(0xff595959);
   static const Color grey56 = Color(0xffE2E2E2);
   static const Color grey57 = Color(0xff9C9C9C);
+  static const Color bg = Color(0xffF0F2F5);
 }
